e056c00638456f5c467cb3d89c8199b96c9bc523
codonculous <feidameng01@gmail.com>
2017-05-01 14:21:40 -0400

    create button to request ping and add the response data in the web

--- ajax.js
+++ ajax.js
@@ -13,4 +13,22 @@ $(function () {
 
   });
 
+  $('#ping-pong').click(function() {
+
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/ping',
+      method: 'GET',
+      data: {},
+      dataType: 'html'
+    }).done(function(data){
+      console.log('data: ping');
+      console.log('data: pong');
+      console.log('Response Data'+ data);
+      $('#step3456').append('<p>'+data+'</p>');
+
+    });
+
+  });
+
+
 });
