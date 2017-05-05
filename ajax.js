e79d7594d80455c82002e21199ba0f915dfe5007
codonculous <feidameng01@gmail.com>
2017-05-05 17:13:42 -0400

    add form to allow user to send timezone to http://first-ajax-api.herokuapp.com/time return the time of cooresponding timezone

--- ajax.js
+++ ajax.js
@@ -38,22 +38,42 @@ $(function () {
   });
 
   $('#count').click(function() {
-
+    for (var i=1;i<10;i++) {
     $.ajax({
-      url: 'http://first-ajax-api.herokuapp.com/count',
-      method: 'GET',
-      data: {},
-      dataType: 'html'
-    }).done(function(data){
-      $('#step7').append('<p>' + data + '</p>');
-    }).fail(function(){
-      var fail = 'Unable to access';
-      console.log(fail);
-      $('#step7').append('<p>'+fail+'</p>');
-    }).always(function(){
-      console.log('Request finished');
-    });
 
+
+        url: 'http://first-ajax-api.herokuapp.com/count',
+        method: 'GET',
+        data: {amount: i },
+        dataType: 'html',
+        context:this,
+        success: this.mySuccess,
+        error:this.myError,
+        cache:false,
+        beforeSend:function(jqXHR,settings){
+          jqXHR.url = settings.url;
+        },
+        error:function(jqXHR,exception){
+          console.log(jqXHR.url);
+          var fail = ' Unable to access';
+          // debugger
+          // console.log(fail);
+          $('#step7').append('<p>'+jqXHR.url+""+fail+'</p>');
+
+        }
+
+      }).done(function(data){
+        $('#step7').append('<p>' + data + '</p>');
+      }).fail(function(error,message,t){
+          // console.log(i,error);
+        // console.log(error);
+
+      }).always(function(b){
+        // debugger
+        // console.log(b+'Request finished');
+      });
+
+    }
   });
 
   $('#time').click(function() {
@@ -69,6 +89,28 @@ $(function () {
       var fail = 'Unable to access';
       console.log(fail);
       $('#step8').append('<p>'+fail+'</p>');
+    }).always(function(){
+      console.log('Request finished');
+    });
+
+  });
+console.log('fasdf');
+  $('#timezone-form').on('click','#submit-b',function(e) {
+    e.preventDefault();
+    var time = $('#timezone-form').find('[name="timezone"]');
+    console.log(this);
+    // debugger
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/time',
+      method: 'GET',
+      data: {timezone : time.val()},
+      dataType: 'html'
+    }).done(function(data){
+      $('#step8').append('<p>' + data + '</p>');
+    }).fail(function(jsXHR,textStatus,errorThrown){
+      var fail = 'Unable to access';
+      console.log(fail);
+      $('#step8').append('<p>'+errorThrown+'</p>');
     }).always(function(){
       console.log('Request finished');
     });
